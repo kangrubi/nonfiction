f8aa9bfa8945fab28e17cefe6267db011793ee4f
kangrubi <rubi0624@nate.com>
2022-12-23 01:05:23 +0900

    로그인 useForm으로 수정

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Login.scss';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { auth } from '../lib/firebase';
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const {
+    watch,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const email = watch('email');
+  const password = watch('password');
 
   const navigate = useNavigate();
 
-  const signIn = e => {
-    e.preventDefault(); // 새로고침 막기
+  const signIn = () => {
     auth
       .signInWithEmailAndPassword(email, password)
       .then(auth => {
@@ -21,9 +28,8 @@ function Login() {
       });
   };
 
-  const register = e => {
+  const join = e => {
     e.preventDefault();
-
     auth
       .createUserWithEmailAndPassword(email, password)
       .then(auth => {
@@ -40,27 +46,47 @@ function Login() {
     <div className="login">
       <div className="login-container">
         <h2>로그인</h2>
-
-        <form>
+        <form onSubmit={handleSubmit(signIn)}>
           <h3>이메일</h3>
           <input
-            value={email}
-            onChange={e => setEmail(e.target.value)}
             type="text"
+            {...register('email', {
+              required: {
+                value: true,
+                message: '이메일을 입력해주세요.',
+              },
+              pattern: {
+                value: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+                message: '잘못된 이메일 주소입니다.',
+              },
+            })}
           />
+          <span className="error-message">
+            {errors.email && errors.email.message}
+          </span>
           <h3>비밀번호</h3>
           <input
-            value={password}
-            onChange={e => setPassword(e.target.value)}
             type="password"
+            {...register('password', {
+              required: {
+                value: true,
+                message: '비밀번호를 입력해주세요.',
+              },
+              pattern: {
+                value:
+                  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+                message:
+                  '최소 8자 이상으로 영문자 대문자, 영문자 소문자, 숫자, 특수문자가 각각 최소 1개 이상 입력해주세요',
+              },
+            })}
           />
-
-          <button onClick={signIn} className="login-signin-button">
-            로그인 하기
-          </button>
+          <span className="error-message">
+            {errors.password && errors.password.message}
+          </span>
+          <button className="login-signin-button">로그인 하기</button>
         </form>
         <p>아직 회원이 아니십니까?</p>
-        <button onClick={register} className="login-register-button">
+        <button onClick={join} className="login-register-button">
           회원가입
         </button>
       </div>
